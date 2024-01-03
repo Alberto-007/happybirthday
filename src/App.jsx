1edbace49ce8b5ed17d4bccb97cfd26997acada2
Alberto-007 <asj3@aluno.ifal.edu.br>
2024-01-03 14:59:05 -0300

    ballons and confettis added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import './App.css'
+import Confetti from 'react-confetti'
+import { useReward } from 'react-rewards'
 
 function App() {
-
+  const {reward} = useReward('rewardId', 'balloons');
   return (
     <>
-
-    <div className='birthday-card'>
+    <center><span id="rewardId" /></center>
+    <div className='birthday-card' onClick={reward}>
       <div className='cover-inner'>
         <img src='https://images.thortful.com/cdn-cgi/image/width=600,format=auto,quality=90/card/58d6b6eee4b0af790da89a19/58d6b6eee4b0af790da89a19_medium.jpg?version=9' 
         width={"100%"} />
       </div>
 
-
       <div className='cover-outer'>
         <img src="https://images.esellerpro.com/3274/I/318/37/lrgscaleAO002.jpg" 
         width={"100%"} />
       </div>
-
 
       <div className='text'>
         <p className='title'>
@@ -29,6 +29,7 @@ function App() {
         <p>Espero que não só hoje mas essa semana seja incrivel!</p>
       </div>
     </div>
+    <Confetti />
     </>
   )
 }
